3bba4e181a737123188c91fbb6fb7b8431694663
Victor-hys <victorhiroshi.y.s@gmail.com>
2020-12-24 16:20:15 -0300

    rename variables

--- main.py
+++ main.py
@@ -4,60 +4,60 @@ import json
 
 app = Flask(__name__)
 
-with open('MarvelApi.json', 'r') as MarvelApi:
-    input_json = MarvelApi.read()
+with open('MarvelApi.json', 'r') as marvelApi:
+    inputJson = marvelApi.read()
 
-input_dict = json.loads(input_json)
+inputDict = json.loads(inputJson)
 
 
 @app.route('/v1/public/characters', methods=['GET'])
 def getAllCharacters():
-    return jsonify(input_dict)
+    return jsonify(inputDict)
 
 
 @app.route('/v1/public/characters/<characterId>', methods=['GET'])
 def getCharacterById(characterId):
-    for element in input_dict:
+    for element in inputDict:
         if element["characterId"] == characterId:
-            output_dict = element
-    output_json = json.dumps(output_dict)
-    return output_json
+            outputDict = element
+    outputJson = json.dumps(outputDict)
+    return jsonify(outputJson)
 
 
 @app.route('/v1/public/characters/<characterId>/comics', methods=['GET'])
 def getComicsByCharacterId(characterId):
-    for element in input_dict:
+    for element in inputDict:
         if element["characterId"] == characterId:
-            output_dict = element["comics"]
-    output_json = json.dumps(output_dict)
-    return output_json
+            outputDict = element["comics"]
+    outputJson = json.dumps(outputDict)
+    return jsonify(outputJson)
 
 
 @app.route('/v1/public/characters/<characterId>/events', methods=['GET'])
 def getEventsByCharacterId(characterId):
-    for element in input_dict:
+    for element in inputDict:
         if element["characterId"] == characterId:
-            output_dict = element["events"]
-    output_json = json.dumps(output_dict)
-    return output_json
+            outputDict = element["events"]
+    outputJson = json.dumps(outputDict)
+    return jsonify(outputJson)
 
 
 @app.route('/v1/public/characters/<characterId>/series', methods=['GET'])
 def getSeriesByCharacterId(characterId):
-    for element in input_dict:
+    for element in inputDict:
         if element["characterId"] == characterId:
-            output_dict = element["series"]
-    output_json = json.dumps(output_dict)
-    return output_json
+            outputDict = element["series"]
+    outputJson = json.dumps(outputDict)
+    return jsonify(outputJson)
 
 
 @app.route('/v1/public/characters/<characterId>/stories', methods=['GET'])
 def getStoriesByCharacterId(characterId):
-    for element in input_dict:
+    for element in inputDict:
         if element["characterId"] == characterId:
-            output_dict = element["stories"]
-    output_json = json.dumps(output_dict)
-    return output_json
+            outputDict = element["stories"]
+    outputJson = json.dumps(outputDict)
+    return jsonify(outputJson)
 
 
 if __name__ == "__main__":
