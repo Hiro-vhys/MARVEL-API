16b54276b7457964bd8e78c4ddd908c922d4b66e
Victor-hys <victorhiroshi.y.s@gmail.com>
2020-12-24 22:09:11 -0300

    error treatment

--- main.py
+++ main.py
@@ -5,59 +5,99 @@ import json
 app = Flask(__name__)
 
 with open('MarvelApi.json', 'r') as marvelApi:
-    inputJson = marvelApi.read()
+    input_json = marvelApi.read()
 
-inputDict = json.loads(inputJson)
+input_dict = json.loads(input_json)
 
+@app.route('/', methods=['GET'])
+def defaultRoute():
+    index = '<h1>Endpoints disponiveis</h1><br>'
+    index += '<p>characterId 1, 2, 3, 4 or 5</p>'
+    index += '<p>/v1/public/characters</p>'
+    index += '<p>/v1/public/characters/characterId/comics</p>'
+    index += '<p>/v1/public/characters/characterId/events</p>'
+    index += '<p>/v1/public/characters/characterId/series</p>'
+    index += '<p>/v1/public/characters/characterId/stories</p>'
+    return index
 
 @app.route('/v1/public/characters', methods=['GET'])
 def getAllCharacters():
-    return jsonify(inputDict)
+    return jsonify(input_dict)
 
 
 @app.route('/v1/public/characters/<characterId>', methods=['GET'])
 def getCharacterById(characterId):
-    for element in inputDict:
-        if element["characterId"] == characterId:
-            outputDict = element
-    outputJson = json.dumps(outputDict)
-    return jsonify(outputJson)
+    if type(characterId) == int:
+        if characterId > 0 and characterId < 6:
+            for element in input_dict:
+                if element["characterId"] == characterId:
+                    output_dict = element
+            output_json = json.dumps(output_dict)
+            return jsonify(output_json)
+        else:
+            return '<p>Enter a number between 1 and 5</p>'
+    else:
+        return '<p>Only integers are allowed</p>'
 
 
 @app.route('/v1/public/characters/<characterId>/comics', methods=['GET'])
 def getComicsByCharacterId(characterId):
-    for element in inputDict:
-        if element["characterId"] == characterId:
-            outputDict = element["comics"]
-    outputJson = json.dumps(outputDict)
-    return jsonify(outputJson)
+    if type(characterId) == int:
+        if characterId > 0 and characterId < 6:
+            for element in input_dict:
+                if element["characterId"] == characterId:
+                    output_dict = element["comics"]
+            output_json = json.dumps(output_dict)
+            return jsonify(output_json)
+        else:
+            return '<p>Enter a number between 1 and 5</p>'
+    else:
+        return '<p>Only integers are allowed</p>'
 
 
 @app.route('/v1/public/characters/<characterId>/events', methods=['GET'])
 def getEventsByCharacterId(characterId):
-    for element in inputDict:
-        if element["characterId"] == characterId:
-            outputDict = element["events"]
-    outputJson = json.dumps(outputDict)
-    return jsonify(outputJson)
+    if type(characterId) == int:
+        if characterId > 0 and characterId < 6:
+            for element in input_dict:
+                if element["characterId"] == characterId:
+                    output_dict = element["events"]
+            output_json = json.dumps(output_dict)
+            return jsonify(output_json)
+        else:
+            return '<p>Enter a number between 1 and 5</p>'
+    else:
+        return '<p>Only integers are allowed</p>'
 
 
 @app.route('/v1/public/characters/<characterId>/series', methods=['GET'])
 def getSeriesByCharacterId(characterId):
-    for element in inputDict:
-        if element["characterId"] == characterId:
-            outputDict = element["series"]
-    outputJson = json.dumps(outputDict)
-    return jsonify(outputJson)
+    if type(characterId) == int:
+        if characterId > 0 and characterId < 6:
+            for element in input_dict:
+                if element["characterId"] == characterId:
+                    output_dict = element["series"]
+            output_json = json.dumps(output_dict)
+            return jsonify(output_json)
+        else:
+            return '<p>Enter a number between 1 and 5</p>'
+    else:
+        return '<p>Only integers are allowed</p>'
 
 
 @app.route('/v1/public/characters/<characterId>/stories', methods=['GET'])
 def getStoriesByCharacterId(characterId):
-    for element in inputDict:
-        if element["characterId"] == characterId:
-            outputDict = element["stories"]
-    outputJson = json.dumps(outputDict)
-    return jsonify(outputJson)
+    if type(characterId) == int:
+        if characterId > 0 and characterId < 6:
+            for element in input_dict:
+                if element["characterId"] == characterId:
+                    output_dict = element["stories"]
+            output_json = json.dumps(output_dict)
+            return jsonify(output_json)
+        else:
+            return '<p>Enter a number between 1 and 5</p>'
+    else:
+        return '<p>Only integers are allowed</p>'
 
 
 if __name__ == "__main__":
